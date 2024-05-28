f0cf21d2aa901a8d7dc19ff9e1f71562c3153279
SindreMA <SindreMA@gmail.com>
2024-05-28 20:32:59 +0200

    added env handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::env;
+
 use aide::{
     axum::ApiRouter,
     openapi::{Contact, Info, OpenApi},
 };
+use dotenv::dotenv;
 use tower_http::services::ServeDir;
-
+extern crate dotenv;
 
 use axum::Extension;
 
@@ -12,12 +15,23 @@ mod endpoints;
 
 #[tokio::main]
 async fn main() {
+    print!("Starting server..");
+
+    print!("Loading .env file..");
+    let env = dotenv().ok().expect("Failed to load .env file");
+
+    let sql_server = env::var("SQL_SERVER").expect("SQL_SERVER not found in .env file");
+    let sql_user = env::var("SQL_USER").expect("SQL_USER not found in .env file");
+    let sql_password = env::var("SQL_PASSWORD").expect("SQL_PASSWORD not found in .env file");
+
+    let port = env::var("PORT").expect("PORT not found in .env file");
+
     let app =  ApiRouter::new()
     .merge(swagger::route_swagger())
     .merge(endpoints::config::create_endpoints())
     .nest_service("/statics", ServeDir::new("src/statics"));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:".to_string() + &port).await.unwrap();
 
     let mut api = OpenApi {
         info: Info {
